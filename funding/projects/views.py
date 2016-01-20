967e2333096897139382094a859df3eab13eb03f
Jay <ja.geb@me.com>
2016-01-20 15:22:59 +0100

    make the create form actually work

--- funding/projects/views.py
+++ funding/projects/views.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from django.views.generic import ListView, CreateView, DetailView
 from .models import Project
+from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
 
 
 class ProjectListView(ListView):
@@ -15,6 +17,11 @@ class ProjectCreateView(CreateView):
     template_name = "projects/create.html"
     fields = ("name", "website", "languages", "function", "maintainers", "added", "needs",
               "funding_status", "funding_status_notes")
+    success_url = reverse_lazy("projects:list")
+
+    def form_valid(self, form):
+        messages.info(self.request, "Project added")
+        return super(ProjectCreateView, self).form_valid(form)
 
 
 class ProjectDetailView(DetailView):
