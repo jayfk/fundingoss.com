bb34c5a3ac942fb0faabf292472c357f8031ffb2
Jay <ja.geb@me.com>
2016-01-20 14:42:21 +0100

    reworked design

--- funding/projects/urls.py
+++ funding/projects/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ProjectListView.as_view(), name="list"),
+    url(r'^create/$', views.ProjectCreateView.as_view(), name="detail"),
     url(r'^create/$', views.ProjectCreateView.as_view(), name="create"),
 ]
