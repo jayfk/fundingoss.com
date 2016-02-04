0c4de324157aa60f3fe2b584c6598309b4626408
Jannis Gebauer <ja.geb@me.com>
2016-02-04 12:18:02 +0100

    display the funding_type as checkboxes in the admin

--- funding/projects/admin.py
+++ funding/projects/admin.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 from django.contrib import admin
+from django import forms
 from .models import Project
+
+
+class ProjectAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        exclude = ()
+
+    funding_type = forms.MultipleChoiceField(
+        choices=Project.FUNDING_TYPE_CHOICES,
+        widget=forms.CheckboxSelectMultiple(),
+        required=False
+    )
 
 
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
+    form = ProjectAdminForm
     list_display = (
         "name", "funding_status", "license", "added", "website"
     )
